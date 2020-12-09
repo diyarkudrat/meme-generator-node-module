8077ab8cd4bfc217a51330e853b0c332cb8998b9
Diyar Kudrat <dkudrat@gmail.com>
2020-12-09 00:48:52 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const request = require('request').defaults({encoding: null});
+const Canvas = require('canvas');
+
+function CustomMemeGenerator (config = {}) {
+
+}
+
+module.exports = {
+    CustomMemeGenerator
+}

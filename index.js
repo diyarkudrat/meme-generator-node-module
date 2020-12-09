82d8425a970b731db083c454cebbb015bde732d1
Diyar Kudrat <dkudrat@gmail.com>
2020-12-09 00:55:20 -0800

    set Meme class

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const request = require('request').defaults({encoding: null});
 const Canvas = require('canvas');
 
+
+
+
 function CustomMemeGenerator (config = {}) {
+
+}
+
+CustomMemeGenerator.prototype.createCanvas = function (options) {
+
+}
+
+CustomMemeGenerator.prototype.setFontOptions = function (options) {
+
+}
+
+CustomMemeGenerator.prototype.setImageOptions = function (options) {
+
+}
+
+CustomMemeGenerator.prototype.generateMeme = function (imgOptions) {
 
 }
 
